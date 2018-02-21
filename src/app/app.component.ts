4c9e4b69df12e212587d20d287f12c0e98d88ca0
OmriBar-Zik <omri@bar-zik.com>
2018-02-21 22:43:09 +0200

    added a bootstrap card in the card body added: an img element and 3 button the first button get an image from cataas api if pressed again request another image the second button get a gif from cataas api if pressed again request another gif the third button get an cute image / gif of a cat with the caption: "love you!" if pressed again request another cute image / gif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  URL = 'https://cataas.com/cat';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  antherPicture() {
+    this.URL = 'https://cataas.com/cat?' + new Date().getTime();
+  }
+
+  antherCutePicture() {
+    const text = 'love you!';
+    this.URL = 'https://cataas.com/cat/cute/says/' + text + '?' + new Date().getTime();
+  }
+
+  anthergif() {
+    this.URL = 'https://cataas.com/cat/gif?' + new Date().getTime();
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
 
@@ -10,9 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
